45cf7d0fbc721e51fc593bc5bdfe66e54db01529
ibrahim <ibrahimbilesanmi@yahoo.com>
2022-09-07 17:57:18 -0500

    pexels Model Classes

--- src/main/java/Services/PexelsApiService.java
+++ src/main/java/Services/PexelsApiService.java
@@ -1,5 +1,13 @@
 package Services;
 
+import java.awt.PageAttributes.MediaType;
+import java.net.http.HttpHeaders;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class PexelsApiService {
 
 }
---
+++ src/main/java/Services/Photos.java
@@ -0,0 +1,9 @@
+package Services;
+
+import java.util.List;
+
+public class Photos {
+	 Integer id;
+	 PhotoSRC src;
+	
+}
