45f3d55ca7a0924e0c0f5ca3733f9ba334e77248
sam <samantha.underwood@gmail.com>
2022-09-10 13:11:29 -0400

    commit to add a missing import

--- src/main/java/co/grandcircus/FinalProject/Controllers/ViewsController.java
+++ src/main/java/co/grandcircus/FinalProject/Controllers/ViewsController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
