f1e9845a5fdf46674e09ce1c56f2bc8828023c61
sam <samantha.underwood@gmail.com>
2022-09-16 16:34:17 -0400

    merging keyword API with main

--- src/main/java/co/grandcircus/FinalProject/Controllers/ViewsController.java
+++ src/main/java/co/grandcircus/FinalProject/Controllers/ViewsController.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
