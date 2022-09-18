3a39e79e4184acb9fa6bbbdcc41f39140c8738cf
sam <samantha.underwood@gmail.com>
2022-09-18 11:53:03 -0400

    merge to comment out some unused elements

--- src/main/java/co/grandcircus/FinalProject/Services/AdventureDBService.java
+++ src/main/java/co/grandcircus/FinalProject/Services/AdventureDBService.java
@@ -103,71 +103,71 @@ public class AdventureDBService {
 
 	}
 
-	// can this be moved out of the api service
-	public Scene setPathLength(Scene scene) {
-
-		if (scene.getChildList() == null) {
-			return scene;
-		}
-	
-		for(Scene s : scene.getChildList()) {
-				
-			int pathLength = getScenePathLength(s);
-			s.setPathLength(pathLength);
-
-			
-			setPathBool(scene.getChildList());
-
-		}
-		
-		return scene;
-	}
-	// could the path functionality move out of the controller while keeping the tree function?
-	private int getScenePathLength(Scene scene) {
-
-		int pathLength = 0;
-
-		List<Scene> childList = getScene(scene.getId()).getChildList();
-		scene.setChildList(childList);
-		
-		if (scene.getChildList() == null) {
-
-			return pathLength;
-		}
-
-		for (Scene s : scene.getChildList()) {
-
-			pathLength = Math.max(pathLength, getScenePathLength(s));
-		}
-
-		return pathLength + 1;
-	}
-	//path boolean method
-	
-	private static void setPathBool(List<Scene> childList) {
-		for(Scene s : childList) {
-			s.setLongest(false);
-			s.setShortest(false);
-		}
-		Scene shortest = childList.get(0);
-		Scene longest = childList.get(0);
-		for(Scene s : childList) {
-			if(shortest.getPathLength() > s.getPathLength()) {
-				shortest = s;
-			}
-			if(longest.getPathLength() < s.getPathLength()) {
-				longest = s;
-			}
-		}
-		for(Scene s : childList) {
-			if(shortest.getPathLength() == s.getPathLength()) {
-				s.setShortest(true);
-			}
-			if(longest.getPathLength() == s.getPathLength()) {
-				s.setLongest(true);
-			}
-		}
-		
-	}
+//	// can this be moved out of the api service
+//	public Scene setPathLength(Scene scene) {
+//
+//		if (scene.getChildList() == null) {
+//			return scene;
+//		}
+//	
+//		for(Scene s : scene.getChildList()) {
+//				
+//			int pathLength = getScenePathLength(s);
+//			s.setPathLength(pathLength);
+//
+//			
+//			setPathBool(scene.getChildList());
+//
+//		}
+//		
+//		return scene;
+//	}
+//	// could the path functionality move out of the controller while keeping the tree function?
+//	private int getScenePathLength(Scene scene) {
+//
+//		int pathLength = 0;
+//
+//		List<Scene> childList = getScene(scene.getId()).getChildList();
+//		scene.setChildList(childList);
+//		
+//		if (scene.getChildList() == null) {
+//
+//			return pathLength;
+//		}
+//
+//		for (Scene s : scene.getChildList()) {
+//
+//			pathLength = Math.max(pathLength, getScenePathLength(s));
+//		}
+//
+//		return pathLength + 1;
+//	}
+//	//path boolean method
+//	
+//	private static void setPathBool(List<Scene> childList) {
+//		for(Scene s : childList) {
+//			s.setLongest(false);
+//			s.setShortest(false);
+//		}
+//		Scene shortest = childList.get(0);
+//		Scene longest = childList.get(0);
+//		for(Scene s : childList) {
+//			if(shortest.getPathLength() > s.getPathLength()) {
+//				shortest = s;
+//			}
+//			if(longest.getPathLength() < s.getPathLength()) {
+//				longest = s;
+//			}
+//		}
+//		for(Scene s : childList) {
+//			if(shortest.getPathLength() == s.getPathLength()) {
+//				s.setShortest(true);
+//			}
+//			if(longest.getPathLength() == s.getPathLength()) {
+//				s.setLongest(true);
+//			}
+//		}
+//		
+//	}
 
 }
